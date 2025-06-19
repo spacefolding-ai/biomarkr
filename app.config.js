e8ed937e3c41b9fe2f117d2fad8e32db78ec809c
Miroslav Jugovic <jugovic.miroslav@gmail.com>
2025-06-19 22:48:41 +0200

    Change name in root dir

--- app.config.js
+++ app.config.js
@@ -2,13 +2,13 @@ import "dotenv/config";
 
 export default {
   expo: {
-    name: "HealthIQ",
-    slug: "healthiq",
+    name: "Biomarkr",
+    slug: "biomarkr",
     version: "1.0.0",
     extra: {
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
     },
-    scheme: "healthiq",
+    scheme: "biomarkr",
   },
 };
